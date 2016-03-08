0d7fbbf77b1f573032559c5cd658923a0fbf69f7
catb0t <thebinaryminer@gmail.com>
2016-03-08 14:05:48 -0500

    rewrite + improve

--- fuzzy.py
+++ fuzzy.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-from string  import punctuation
-from difflib import SequenceMatcher as seqman
+from __future__ import division
+from string  import punctuation as punc
+from difflib import SequenceMatcher as seqmat
 
+DEBUG = True
 
 class Match():
 
     def __init__(self, line, line_no, match_type, prectxt, postctxt, misc=None):
-        self.line, self.line_no, self.match_type, self.prectxt, self.postctxt = line, line_no, match_type, prectxt, postctxt
-
-        self.misc_data = misc
+        (self.line, self.line_no,
+            self.match_type, self.prectxt,
+                self.postctxt, self.misc_data) = (line, line_no,
+                                    match_type, prectxt, postctxt, misc)
 
         self.matchinfo = (self.line, self.line_no, self.match_type,
                           self.prectxt, self.postctxt, self.misc_data)
@@ -18,86 +21,107 @@ class Match():
     def misc(self): return self.misc_data
 
 
-def _sort_and_join(s):
-    x = set("".join(sorted(set(list(s)))))
-    return [x, "".join(x)]
+def is_subset(a, b):
+    """a slow (iterative) dupe-preserving sorting "set",
+    for mutual membership tests (set overloads &, but removes dupes)"""
+    c = []
+    a, b = sorted(list(a)), sorted(list(b))
+    (l, maxl, s, minl) = {
+        True:  (a, len(a), b, len(b)),
+        False: (b, len(b), a, len(a))
+    }[ a >= b ]
 
+    for i in range(maxl):
+        try:
+            if s.count(l[i]) >= l.count(l[i]):
+                c.append(a[i])
 
-def fuzzy_files(needle, haystack_of_files, tolerance=.4, context_lines=2, punc_is_junk=True, junk_func=None, case_sens=case_sens):
+        except IndexError:
+            pass
+    return c
+
+def fuzzy_files(needle, file_haystack, **kwargs):
+    """fuzzy grep in files. turns kwargs in to fuzzy_files"""
 
     metamatches = {}
 
-    for f in haystack_of_files:
+    for fname in file_haystack:
 
-        t = open(f, "r")
-        c = t.read()
-        t.close()
+        fio = open(fname, "r")
+        fct = fio.read()
+        fio.close()
 
-        metamatches[f] = fuzzy_grep(
-            needle, c, tolerance=tolerance, context_lines=context_lines, punc_is_junk=punc_is_junk, junk_func=junk_func, case_sens=case_sens)
+        metamatches[fname] = fuzzy_grep(needle, fct, **kwargs)
 
     return metamatches
 
 
-def fuzzy_grep(needle, haystack, tolerance=.4, context_lines=2, punc_is_junk=True, junk_func=None, case_sens=False):
+def fuzzy_grep(needle,       haystack,
+        TOLERANCE_BASE=.3,   CONTEXT_LINES=2,
+        PUNC_IS_JUNK=True,   JUNK_FUNC=None,
+        CONSIDER_CASE=False, ADJUST_BYLEN=True, APPROX_THRESHOLD=.45):
     """fuzzily grep, finding needle in haystack.split('\n')
-    tolerance     = levenshtein tolerance for SequenceMatcher ratio     -- float or int default: .4
-    context_lines = lines of context surrounding each match to supply   -- int          default: 2
-    punc_is_junk  = whether to consider string.punctuation in fuzziness -- bool         default: True
-    junk_func     = a caller-supplied junk-decider                      -- function     default: None
-    case_sens     = whether case should be considered in matches        -- bool         default: False"""
+    tolerance_base = base levenshtein tolerance for seqman ratio         :float default: .4
+    context_lines  = lines of context surrounding each match to supply   :int   default: 2
+    punc_is_junk   = whether to consider string.punctuation in fuzziness :bool  default: True
+    junk_func      = a caller-supplied junk-decider                      :func  default: None
+    case_sens      = should case be considered in matches                :bool  default: False
+    adjust_bylen   = adjust tolerance using ratio of needle to line len  :bool  default: True"""
 
     matches = []
 
-    if not case_sens:
-        needle = needle.lower()
+    CONTEXT_LINES += 1
 
-    if punc_is_junk:
-        junk = lambda x: set(punctuation) & set(x)
-    elif junk_func:
-        junk = junk_func
+    if PUNC_IS_JUNK:
+        junk = (
+            lambda x: set(punc) & set(x)
+        )
+    elif JUNK_FUNC is not None:
+        junk = JUNK_FUNC
     else:
-        junk = lambda x: False
+        junk = (lambda x: False)
 
-    s_txt, js_txt = _sort_and_join(needle)
+    if not CONSIDER_CASE:
+        needle   = needle.lower()
+        haystack = haystack.lower()
 
-    lns = haystack.split("\n")
+    ls = haystack.split("\n")
 
-    for num, line in enumerate(lns):
+    for idx, line in enumerate(ls):
 
-        if not case_sens:
-            line = line.lower()
+        tolerance = TOLERANCE_BASE
 
-        s_line, _ = _sort_and_join(line)
+        if ADJUST_BYLEN:
+            try:
+                coef = len(needle) / len(line)
+            except ZeroDivisionError:
+                coef = 0
+            tolerance = round(tolerance + tolerance * (coef * 2), 2)
 
-        hasletters = "".join(s_txt & s_line)
+        fuzziness = is_subset(needle, line)
 
-        s = seqman(
+        s = seqmat(
             junk,
             line,
             needle
         )
-
-        ratio  = s.ratio()
-        exact  = needle in line
-        approx = round(ratio + tolerance)
-        found  = exact or approx
-        if found and hasletters == js_txt:
+        ratio = s.ratio()
+        exact = (needle in line) or ("".join(sorted(needle)) in "".join(sorted(line)))
+        apprx = ratio + tolerance
+        found = exact or apprx > APPROX_THRESHOLD
+        #print(round(ratio, 2), round(tolerance, 2), round(apprx, 2), bool(apprx), bool(found))
+        if found and fuzziness == sorted(needle):
             try:
                 matches.append(
                     Match(
                         line,
-                        num,
+                        idx,
                         "exact" if exact else "fuzzy",
-                        [ ln for ln in [ lns[num - i] for i in range(1, context_lines) ] ],
-                        [ ln for ln in [ lns[num + i] for i in range(1, context_lines) ] ],
+                        [ ln for ln in [ ls[idx - i] for i in range(1, CONTEXT_LINES) ] ],
+                        [ ln for ln in [ ls[idx + i] for i in range(1, CONTEXT_LINES) ] ],
                         misc={
-                            "seqman": {"self": s, "ratio": ratio, "tolerance": tolerance},
-                            "misc": {
-                                "exact": exact, "approx": approx, "found": found, "context_lines": context_lines,
-                                "punc_is_junk": punc_is_junk, "junk_func": junk_func, "junk_decider": junk,
-                                "sorted_txt": s_txt, "sorted_line": s_line, "line_has_letters": hasletters,
-                            }
+                            "seqmat": {"self": s, "ratio": ratio, "tolerance": tolerance, "tolerance_base": TOLERANCE_BASE},
+                            "misc": locals()
                         }
                     )
                 )
@@ -107,8 +131,8 @@ def fuzzy_grep(needle, haystack, tolerance=.4, context_lines=2, punc_is_junk=Tru
 
     return matches
 
+from sys import argv
 def demo():
-    from sys import argv
     output = []
     results = fuzzy_files(argv[1], argv[2:])  # a string as arg #1 and filenames as the rest
 
@@ -123,9 +147,12 @@ def demo():
                     "-" * 100, item.line_no, fname, item.match_type
                 ) + "\n" +
                 "\t" + "\n\t".join(item.prectxt) + "\n"
-                ">>>\t" + item.line + "\n" +
+                "\x1b[1;31m>>>\t" + item.line + "\x1b[m\n" +
                 "\t" + "\n\t".join(item.postctxt) + "\n"
             )
 
     print("".join(output))
 
+if __name__ == '__main__' and DEBUG:
+    demo()
+    print(len(fuzzy_files(argv[1], argv[2:])[argv[2]]))
